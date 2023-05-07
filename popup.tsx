e675f56ce4bc1285d12ada944b14d2ae50ad84f3
niohx <nioh@live.jp>
2023-05-07 21:30:45 +0900

    ittan

--- popup.tsx
+++ popup.tsx
@@ -1,17 +1,25 @@
-import { Button } from "antd";
-import { useEffect, useState } from "react"
-import { Storage } from "@plasmohq/storage"
+import { Button, Form, Input } from "antd";
+import { useStorage } from "@plasmohq/storage/hook"
+import {Storage} from "@plasmohq/storage"
+import React, { useEffect, useState } from "react"
 
 
-const IndexPopup:React.FC=()=> {
-  const [data, setData] = useState(null)
-  useEffect(()=>{
-    const strage = new Storage()
-    
-    strage.get("ip").then((res)=>{
-      console.log(res)
-      setData(res)})
-  });
+
+const IndexPopup: React.FC = () => {
+  const [ipAddress, setIpAddress] = useStorage("ipAddress",(v) => v === null ? "nothing": v)
+  // const loadData = async (storage:Storage) => {
+  //   const data = await storage.get("ipAddress")
+  //   console.log(data)
+  //   setIpAddress(data)
+  // }
+  // const saveToStorage = async (storage:Storage,ipAddress:string) => {
+  //   await storage.set("ipAddress", ipAddress)
+  //   setIpAddress(ipAddress)
+  // }
+  // const storage = new Storage()
+//  useEffect(()=>{
+//   console.log(`IP is ${ipAddress}`)
+//   loadData(storage)})
   return (
     <div
       style={{
@@ -20,19 +28,26 @@ const IndexPopup:React.FC=()=> {
         padding: 16
       }}>
       <h2>
-        Please Enter Your Printer IP Address<p/>
-        <p/>
-        now save on {data!=null?data:"no data saved"}
-        <p/>
-        
+        Please Enter Your Printer IP Address<p />
+        <p />
+        now save on {ipAddress != null ? ipAddress : "no data saved"}
+        <p />
+
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <Button onClick={()=>{
-        const storage = new Storage()
-        storage.set("ip",data)
-      }}>save</Button>
+      <Form onFinish={(values)=>{
+        console.log(`ip is ${values.ipaddress}`)
+        setIpAddress(values.ipaddress)}}>
+        <Form.Item label="IP Address" name="ipaddress">
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit">save</Button>
+        </Form.Item>
+      </Form>
+
+
     </div>
-    
+
   )
 }
 
